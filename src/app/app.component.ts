a17210c40cee7a9ee36d1be7cf62fa7d39013e78
Vienyame EDORH-TOSSA <vienyame.edorhtossa.ext@suez.com>
2024-07-11 23:16:55 +0200

    todo list in app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,22 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TasksComponent } from './tasks/tasks.component';
+import { TodoItem } from './todo/models/todo-item.model';
+import { TodoListService } from './todo/services/todo-list.service';
+import { TodoListComponent } from './todo/todo-list/todo-list.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, TasksComponent],
+    imports: [CommonModule, RouterOutlet, TasksComponent, TodoListComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
 })
 export class AppComponent {
-    title = 'learning-angular-signal';
+    public todoItems: Signal<TodoItem[]>;
+
+    constructor(private readonly todoListService: TodoListService) {
+        this.todoItems = this.todoListService.todoItems;
+    }
 }
