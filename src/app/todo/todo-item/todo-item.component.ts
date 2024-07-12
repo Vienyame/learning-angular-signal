c6ad624ecbc3f0f4f4e8905defc4b319bd943f10
Vienyame EDORH-TOSSA <vienyame.edorhtossa.ext@suez.com>
2024-07-12 21:58:11 +0200

    menu item template

--- src/app/todo/todo-item/todo-item.component.ts
+++ src/app/todo/todo-item/todo-item.component.ts
@@ -1,11 +1,15 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatIconButton } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { MatIcon } from '@angular/material/icon';
 import { TodoItem } from '../models/todo-item.model';
 
 @Component({
     selector: 'app-todo-item',
     standalone: true,
-    imports: [ReactiveFormsModule],
+    imports: [ReactiveFormsModule, MatCard, MatCheckbox, MatIconButton, MatIcon],
     templateUrl: './todo-item.component.html',
     styleUrl: './todo-item.component.scss',
 })
@@ -14,6 +18,11 @@ export class TodoItemComponent {
     @Output() public isCompletedChange = new EventEmitter<boolean>();
     @Output() public delete = new EventEmitter<string>();
     @Output() public edit = new EventEmitter<TodoItem>();
+    public isCompletedFormControl: FormControl = new FormControl();
 
     constructor() {}
+
+    public onDelete() {}
+
+    public onEdit() {}
 }
