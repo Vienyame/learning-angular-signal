fdc8ca2b86bfaacedf066c4576b9d5ca2170a9f0
Vienyame EDORH-TOSSA <vienyame.edorhtossa.ext@suez.com>
2024-07-12 21:57:48 +0200

    update angular material config

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-    providers: [provideRouter(routes), importProvidersFrom([BrowserAnimationsModule])],
+    providers: [provideRouter(routes), importProvidersFrom([BrowserAnimationsModule]), provideAnimationsAsync()],
 };
